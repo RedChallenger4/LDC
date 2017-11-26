2be3b7ad922b805f14a1dac5592d13a2f948fc1e
phanx211 <33839067+phanx211@users.noreply.github.com>
2017-11-26 16:05:08 -0600

    Attempted to make navigation drawer button appear in game menu, didn't work

--- app/src/main/java/app/theducksneezes/com/ldc/GameMenuActivity.java
+++ app/src/main/java/app/theducksneezes/com/ldc/GameMenuActivity.java
@@ -1,11 +1,16 @@
 package app.theducksneezes.com.ldc;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class GameMenuActivity extends AppCompatActivity {
+public class GameMenuActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,34 @@ public class GameMenuActivity extends AppCompatActivity {
     public void openToBlocks(View view){
         Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class );
         startActivity(intent);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+            Intent intent = new Intent(getApplicationContext(), GameMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(getApplicationContext(), CalendarActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_slideshow) {
+            Intent intent = new Intent(getApplicationContext(), GoOutsideMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+            Intent intent = new Intent(getApplicationContext(), WatchMoviesMainMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(getApplicationContext(), ConnectMenuActivity.class );
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
