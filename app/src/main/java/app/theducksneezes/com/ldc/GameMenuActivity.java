95642fb7c9d3725ef489a70c62c18fa1d4b1ed57
phanx211 <33839067+phanx211@users.noreply.github.com>
2017-11-26 18:19:07 -0600

    Swiping right on the game menu brings out navigation drawer
    The header "Long Distance Connections" is on top of the navigation drawer, blocking part of the logo. Need to fix this. Can't yet figure out how to get the hamburger menu icon to show up in the top left of the screen.

--- app/src/main/java/app/theducksneezes/com/ldc/GameMenuActivity.java
+++ app/src/main/java/app/theducksneezes/com/ldc/GameMenuActivity.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
 public class GameMenuActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
+
+
     }
 
     // opens to Whiteboard Activity
@@ -30,32 +34,4 @@ public class GameMenuActivity extends AppCompatActivity
         startActivity(intent);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            Intent intent = new Intent(getApplicationContext(), GameMenuActivity.class );
-            startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-            Intent intent = new Intent(getApplicationContext(), CalendarActivity.class );
-            startActivity(intent);
-        } else if (id == R.id.nav_slideshow) {
-            Intent intent = new Intent(getApplicationContext(), GoOutsideMenuActivity.class );
-            startActivity(intent);
-        } else if (id == R.id.nav_manage) {
-            Intent intent = new Intent(getApplicationContext(), WatchMoviesMainMenuActivity.class );
-            startActivity(intent);
-        } else if (id == R.id.nav_share) {
-            Intent intent = new Intent(getApplicationContext(), ConnectMenuActivity.class );
-            startActivity(intent);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
