5781217b3ac8ef8ea165fd2e5bef6843dcfbd56f
phanx211 <33839067+phanx211@users.noreply.github.com>
2017-11-27 12:40:14 -0600

    Added the nav drawer workaround to the main activity menuss
    -Need to add it to the subactivities
    -Trying to figure out how to put the button on the left side

--- app/src/main/java/app/theducksneezes/com/ldc/WatchMoviesMainMenuActivity.java
+++ app/src/main/java/app/theducksneezes/com/ldc/WatchMoviesMainMenuActivity.java
@@ -3,6 +3,8 @@ package app.theducksneezes.com.ldc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class WatchMoviesMainMenuActivity extends AppCompatActivity {
@@ -23,5 +25,49 @@ public class WatchMoviesMainMenuActivity extends AppCompatActivity {
     public void openToNetflix(View view){
         Intent intent = new Intent(getApplicationContext(), DrawerMenuActivity.class );
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.nav_game) {
+            Intent intent = new Intent(getApplicationContext(), GameMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_schedule) {
+            Intent intent = new Intent(getApplicationContext(), CalendarActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_outside) {
+            Intent intent = new Intent(getApplicationContext(), GoOutsideMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_movie) {
+            Intent intent = new Intent(getApplicationContext(), WatchMoviesMainMenuActivity.class );
+            startActivity(intent);
+        } else if (id == R.id.nav_chat) {
+            Intent intent = new Intent(getApplicationContext(), ConnectActivity.class );
+            intent.putExtra("title", "Chat");
+            startActivity(intent);
+        } else if (id == R.id.nav_call) {
+            Intent intent = new Intent(getApplicationContext(), ConnectActivity.class );
+            intent.putExtra("title", "Phonecall");
+            startActivity(intent);
+        } else if (id == R.id.nav_video) {
+            Intent intent = new Intent(getApplicationContext(), ConnectActivity.class );
+            intent.putExtra("title", "Videocall");
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
